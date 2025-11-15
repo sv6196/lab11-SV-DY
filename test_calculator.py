6fc9fca9b5422b3b9e57730395b23d80024730bb
Sean Vong <svong@ufl.edu>
2025-11-14 20:51:37 -0600

    merged changes

--- test_calculator.py
+++ test_calculator.py
@@ -11,12 +11,16 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+        self.assertEqual(multiply(3,3), 9)
+        self.assertEqual(multiply(1,4), 4)
+        self.assertEqual(multiply(6,7), 42)
+
+    def test_divide(self): # 3 assertions
+        self.assertEqual(divide(3, 3), 1)
+        self.assertEqual(divide(10, 5), 2)
+        self.assertEqual(divide(49, 7), 7)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,21 +38,23 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(math.e, 0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        self.assertAlmostEqual(hypotenuse( 8,6), 10)
+        self.assertAlmostEqual(hypotenuse( 3,4), 16)
+        self.assertAlmostEqual(hypotenuse( 5,12), 13)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ValueError):
+            square_root(-1)
+        with self.assertRaises(ValueError):
+            square_root(-321)
+        with self.assertRaises(ValueError):
+            square_root(-0.232)
+
     ##########################
 
 # Do not touch this
